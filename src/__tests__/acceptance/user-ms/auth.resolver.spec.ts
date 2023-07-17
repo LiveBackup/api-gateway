06e6cec87ecac340705c6105abdc3138f959e34c
Juan Diego <jdiegopm12@gmail.com>
2023-07-17 17:15:27 -0500

    Add a second test for signUp resolver

--- src/__tests__/acceptance/user-ms/auth.resolver.spec.ts
+++ src/__tests__/acceptance/user-ms/auth.resolver.spec.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import sinon from 'sinon';
 import {ApiGateway} from '../../../application';
+import {MsHttpError} from '../../../services/abstract-ms.service';
 import {
   givenClient,
   givenRunningApp,
@@ -39,18 +40,22 @@ describe('e2e - Auth Resolver', () => {
   });
 
   describe('Signup resolver', () => {
-    it('Register a new account', async () => {
+    it('Registers a new account', async () => {
+      // Create the mocks
       const newAccount = givenNewAccount();
       const expectedAccount = givenAccount();
 
+      // Mock the Post method on Axios to return the expectedAccount
       userMsMock.onPost().reply(201, JSON.stringify(expectedAccount));
 
+      // Query the graphql server
       const requestData = parseSignUp(newAccount);
       const response = await queryGraphQL(client, requestData);
 
+      // Compare the expected behavior
       expect(response.statusCode).to.be.equal(200);
       const responseData = response.body.data;
-      expect(responseData.error).to.be.Undefined();
+      expect(response.body.errors).to.be.Undefined();
       expect(responseData.signUp.id).to.be.equal(expectedAccount.id);
       expect(responseData.signUp.username).to.be.equal(
         expectedAccount.username,
@@ -59,6 +64,30 @@ describe('e2e - Auth Resolver', () => {
       expect(responseData.signUp.isEmailVerified).to.be.equal(
         expectedAccount.isEmailVerified,
       );
+    });
+
+    it('Fails to register an account', async () => {
+      // Create the mocks
+      const newAccount = givenNewAccount();
+      const expectedError: MsHttpError = {
+        error: {
+          message: 'There already exists an account with the given email',
+          statusCode: 400,
+        },
+      };
+
+      // Mock the Post method on Axios to return the expectedAccount
+      userMsMock.onPost().reply(400, JSON.stringify(expectedError));
+
+      // Query the graphql server
+      const requestData = parseSignUp(newAccount);
+      const response = await queryGraphQL(client, requestData);
+      // Compare the expected behavior
+      expect(response.statusCode).to.be.equal(200);
+      const responseBody = response.body;
+      expect(responseBody.errors).not.to.be.Undefined();
+      const error = responseBody.errors[0];
+      expect(error.message).to.be.equal(expectedError.error.message);
     });
   });
 });
