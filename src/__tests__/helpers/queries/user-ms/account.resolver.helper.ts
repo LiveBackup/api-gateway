5740b612c77879d06f7d75b802f5c1cc1e6d81d9
Juan Diego <jdiegopm12@gmail.com>
2023-07-27 19:01:11 -0500

    Add the VerifyEmail resolver
    Add the tests over the VerifyEmail resolver

--- src/__tests__/helpers/queries/user-ms/account.resolver.helper.ts
+++ src/__tests__/helpers/queries/user-ms/account.resolver.helper.ts
@@ -16,3 +16,20 @@ export const parseRequestEmailVerification = (): GraphQLBody => {
     `,
   };
 };
+
+export const parseVerifyEmail = (): GraphQLBody => {
+  return {
+    operationName: 'VerifyEmail',
+    query: `
+      mutation VerifyEmail {
+        verifyEmail {
+          id
+          email
+          username
+          isEmailVerified
+          registeredAt
+        }
+      }
+    `,
+  };
+};
